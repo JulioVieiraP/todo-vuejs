bc4e9db061d3b33b4c430d4816a49d436c1899a2
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-04-08 09:55:15 -0300

    tentando arrumar os componentes

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { reactive } from 'vue'
+import Cabeçalho from './components/Cabeçalho'
+import Formulario from "./components/Formulario";
+import Lista from "./components/lista";
 
 const estado = reactive({
   filtro: 'todas',
@@ -58,35 +61,10 @@ const cadastraTarefa = () =>{
 
 <template>
   <div class="container">
-    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
-      <h1>Minhas tarefas</h1>
-      <p>
-        Você possui {{ getTarefasPendentes().length }} tarefas pendentes
-      </p>
-    </header>
-    <form @submit.prevent="cadastraTarefa">
-      <div class="row">
-        <div class="col">
-          <input :value="estado.tarefaTemp" @change="e => estado.tarefaTemp = e.target.value" required type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
-        </div>
-        <div class="col-md-2">
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
-        </div>
-        <div class="col-md-2">
-          <select @change="e => estado.filtro = e.target.value" class="form-control">
-            <option value="todas">Todas as tarefas</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="finalizadas">Finalizadas</option>
-          </select>
-        </div>
-      </div>
-    </form>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
-        <input @change="e => tarefa.finalizada = e.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
-        <label :class="{done : tarefa.finalizada}" class="ms-3" :for="tarefa.titulo"> {{ tarefa.titulo }} </label>
-      </li>
-    </ul>
+    <Cabeçalho tarefa />
+    <Formulario/>
+    <Lista/>
+    
   </div>
 </template>
 
---
+++ todo-vue/src/components/Lista.vue
@@ -0,0 +1,8 @@
+<template>
+    <ul class="list-group mt-4">
+      <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+        <input @change="e => tarefa.finalizada = e.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
+        <label :class="{done : tarefa.finalizada}" class="ms-3" :for="tarefa.titulo"> {{ tarefa.titulo }} </label>
+      </li>
+    </ul>
+</template>
